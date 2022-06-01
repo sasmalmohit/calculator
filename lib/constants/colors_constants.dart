715be7772fb0aa4bf0afd3677908608f8d2214c1
techpassionmohit <106595627+techpassionmohit@users.noreply.github.com>
2022-06-01 09:42:27 +0530

    first commit

---
+++ lib/constants/colors_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kSharkColor = Color.fromARGB(255, 0, 0, 0);
+const kCharadeColor = Color(0xFF292D36);
+const kWhiteColor = Color(0xFFFFFFFF);
+const kAlabasterColor = Color(0xFFF9F9F9);
+const kTurquoiseColor = Color(0xFF25FFDD);
+const kBurntSienna = Color(0xFFEC6666);
